513c5c1b7b42fae02ce16773734f2f2282d8c079
Marcus Saffold <saffo026@d.umn.edu>
2024-02-28 13:55:55 -0600

    Now storing created objects in vectors

--- main.cpp
+++ main.cpp
@@ -2,15 +2,36 @@
 #include "alertReaderFactory.h"
 #include <vector>
 
+/**
+ * File basically serves as a demo of how the factory works. I (Marcus) will probably move this code to the unit test folder.
+ * The code creates three JSON objects and outputs the isParsingCompleted and extractNextDataElement once for each object.
+ * The '1' is output because the Boolean is true
+ *
+ * TODO: Find a way to create json objects with different product, timedate, and message data
+ * */
+
+
 int main()
 {
 
-    // Not currently stored in vectors
-    alertReaderFactory factory;
+alertReaderFactory factory;
 
-    std::shared_ptr <IAlertReader> myFile = factory.createAlertReader("json");
-    myFile->extractNextDataElement();
+// Stores pointers
+std::vector <std::shared_ptr<IAlertReader>> createAlertReader;
+// Stores files created by factory
+std::vector <std::string> fileType {"json", "json", "json"};
 
+for (const auto& fileType : fileType) {
+    auto file = factory.createAlertReader(fileType);
+    if (file != nullptr) {
+        createAlertReader.push_back(file);
+    }
+}
+
+for (const auto& file : createAlertReader) {
+    std::cout << file->isParsingCompleted() << std::endl;
+    file->extractNextDataElement();
+}
 
     return 0;
 }
