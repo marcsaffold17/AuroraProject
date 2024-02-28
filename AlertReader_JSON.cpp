92c9acb127e20300e8fe8f5dd6cd95b77d898d58
Marcus Saffold <saffo026@d.umn.edu>
2024-02-28 13:38:30 -0600

    Changed extractNextDataElement function and added AlertReader_JSON function

--- AlertReader_JSON.cpp
+++ AlertReader_JSON.cpp
@@ -5,9 +5,19 @@
 #include "AlertReader_JSON.h"
 #include <iostream>
 
-// Change definition
-void AlertReader_JSON::extractNextDataElement() {
-    std::cout << "Product ID: " << &product << "\n";
-    std::cout << "Date and Time: " << &timeDate << "\n";
-    std::cout << "Message: " << &message << "\n";
+bool AlertReader_JSON::isParsingCompleted() const {
+    return true;
+}
+
+AlertReader_JSON::AlertReader_JSON()
+: product ("TIIA"), message ("*insert space stuff*"), timeDate("2024-02-02 12:06:50.743")
+{
+
+}
+
+void AlertReader_JSON::extractNextDataElement()
+{
+    std::cout << "Product ID: " << product << "\n";
+    std::cout << "Date and Time: " << timeDate << "\n";
+    std::cout << "Message: " << message << "\n";
 }
