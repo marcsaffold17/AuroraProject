f8a45a34d1bda632baced51bbc3849e3e66e1915
Marcus Saffold <saffo026@d.umn.edu>
2024-02-27 18:58:43 -0600

    added class file

---
+++ alertReaderFactory.cpp
@@ -0,0 +1,20 @@
+//
+// Created by msaff on 2/27/2024.
+//
+
+#include "alertReaderFactory.h"
+#include <AlertReader_JSON.h>
+// Remove when done
+#include <iostream>
+
+std::shared_ptr<IAlertReader> alertReaderFactory::createAlertReader(const std::string &fileType)
+{
+if (fileType == "json")
+    // Remove when done
+    std::cout << "The factory works";
+// Code breaks on the line below
+    // return std::make_shared< AlertReader_JSON > ();
+    //
+else
+    return nullptr;
+}
