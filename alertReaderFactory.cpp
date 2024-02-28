f1c47e060f42b04c6881c63285b0c60cf453aeed
Marcus Saffold <saffo026@d.umn.edu>
2024-02-28 13:36:09 -0600

    Fixed make_shared line

--- alertReaderFactory.cpp
+++ alertReaderFactory.cpp
@@ -4,17 +4,13 @@
 
 #include "alertReaderFactory.h"
 #include <AlertReader_JSON.h>
-// Remove when done
-#include <iostream>
 
 std::shared_ptr<IAlertReader> alertReaderFactory::createAlertReader(const std::string &fileType)
 {
-if (fileType == "json")
-    // Test to see if the "if" statement works correctly. Remove when done
-    std::cout << "The factory works";
-// Code breaks on the line below
-    // return std::make_shared< AlertReader_JSON > ();
-    //
+if (fileType == "json") {
+    return std::make_shared< AlertReader_JSON > ();
+}
+
 else
     return nullptr;
 }
