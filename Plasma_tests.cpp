910c643fd0b9b54695b4270d8a53dbc3dafa5878
Zack Anderson <and08847@d.umn.edu>
2024-03-13 14:46:37 -0500

    Finished plasma file for single observations to convert str to float from plasma reader

--- Plasma_tests.cpp
+++ Plasma_tests.cpp
@@ -1,31 +1,55 @@
 //
 // Created by zebra on 3/12/2024.
 //
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
 #include <iostream>
 #include <vector>
+#include <nlohmann/json.hpp>
+#include <alertReaderFactory.h>
+#include "IAlertReader.h"
+#include "AlertReader_JSON.h"
+#include <string>
+#include <memory>
 
 class Plasma_data {
 private:
-    std::string time_tag;
-    std::string density;
-    std::string speed;
-    std::string temperature;
+    float time_tag;
+    float density;
+    float speed;
+    float temperature;
+    std::vector<float> singleObs; //1st position is time_tag, 2nd is density, 3rd is speed, 4th is temperature
+    //std::vector<std::vector<float>> vectorOfObs; //Vector holding vectors of Observations, use in Satellite storage
 public:
-    std::string getTime_Tag();
-    std::string getDensity();
-    std::string getSpeed();
-    std::string getTemperature();
+    float getTime_Tag();
+    float getDensity();
+    float getSpeed();
+    float getTemperature();
+    float readObs(std::string,std::string,std::string,std::string);
 };
 
-std::string Plasma_data::getTime_Tag() {
+float Plasma_data::getTime_Tag() {
     return time_tag;
 }
-std::string Plasma_data::getDensity() {
+float Plasma_data::getDensity() {
     return density;
 }
-std::string Plasma_data::getSpeed() {
+float Plasma_data::getSpeed() {
     return speed;
 }
-std::string Plasma_data::getTemperature() {
+float Plasma_data::getTemperature() {
     return temperature;
+}
+
+float Plasma_data::readObs(std::string tt_in, std::string d_in, std::string s_in, std::string temp_in) {
+    time_tag=std::stof(tt_in);
+    density=std::stof(d_in);
+    speed=std::stof(s_in);
+    temperature=std::stof(temp_in);
+    singleObs.push_back(time_tag);
+    singleObs.push_back(density);
+    singleObs.push_back(speed);
+    singleObs.push_back(temperature);
+    //vectorOfObs.push_back(singleObs); To be used in Satellite storage
 }
