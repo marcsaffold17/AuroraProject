ba2a7d821d1a6877bf5e7c8fe872b9e702051ea6
Austin Gore <gore0117@d.umn.edu>
2024-03-16 13:26:04 -0600

    Added Satellite Data class and first tests of constructor and data getters/setters. More to come later

---
+++ Satellites.cpp
@@ -0,0 +1,29 @@
+//
+// Created by vikin on 3/15/2024.
+//
+
+#include "Satellites.h"
+std::shared_ptr<Satellites> Satellites::getSingleSatObs(const std::string given_issue_dateTime)
+{
+    int numSats = AllSatellites.size();
+    bool satInArray = false;
+    for (int i = 0; i < numSats; i++){
+        if (AllSatellites[i]->getIssueDateTime() == given_issue_dateTime){
+            satInArray = true;
+            return AllSatellites[i];
+        }
+    }
+    if (satInArray == false){
+        std::shared_ptr<SingleSatellite> newSat;
+        newSat->setIssueDateTime(given_issue_dateTime);
+        AllSatellites.push_back(newSat);
+    }
+    return AllSatellites[numSats - 1];
+}
+void Satellites::addSatellite(std::string given_issue_dateTime)
+{
+    std::shared_ptr<SingleSatellite> newSat;
+    newSat->setIssueDateTime(given_issue_dateTime);
+    AllSatellites.push_back(newSat);
+
+}
