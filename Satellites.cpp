f79f1c372e757e1a14394f5fadda6e73275d9474
Austin Gore <gore0117@d.umn.edu>
2024-03-17 13:44:07 -0500

    Added doxygen style comments to Alert and Satellite classes

--- Satellites.cpp
+++ Satellites.cpp
@@ -3,7 +3,9 @@
 //
 
 #include "Satellites.h"
-#include <math.h>
+///
+/// \param given_issue_dateTime
+/// \return
 Satellite Satellite::getSingleSatObs(const std::string given_issue_dateTime)
 {
     int numSats = AllSatellites.size();
@@ -22,15 +24,20 @@ Satellite Satellite::getSingleSatObs(const std::string given_issue_dateTime)
     return AllSatellites[numSats - 1];
 }
 
+///
+/// \param newSat
 void Satellite::addSatellite(Satellite newSat)
 {
     AllSatellites.push_back(newSat);
 }
 
+///
+/// \return
 float Satellite::getDistance()
 {
     float x_component = (bx_gsm - bx_gse)*(bx_gsm - bx_gse);
     float y_component = (by_gsm - by_gse)*(by_gsm - by_gse);
     float z_component = (bz_gsm - bz_gse)*(bz_gsm - bz_gse);
-    return sqrt((x_component + y_component + z_component));
+    float Distance = sqrt((x_component + y_component + z_component));
+    return Distance;
 }
